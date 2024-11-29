74d9f08185c3e8fb8b704f713134694ce5159553
Xyang-git <tanxuyang2019@gmail.com>
2024-11-29 19:17:17 +0000

    update

--- Project1/data_structures.h
+++ Project1/data_structures.h
@@ -1,9 +1,10 @@
 #ifndef DATA_STRUCTURES_H
 #define DATA_STRUCTURES_H
 #include "data_structures.c"
-typedef struct Node;
+
 Node* createNode(int data);
 int getData(Node* node);
 Node* gotoNextNode(Node* node);
 Node* gotoPreviousNode(Node* node);
+
 #endif
